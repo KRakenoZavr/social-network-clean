7a635ef0734ad814f9f81329a001646d6ea60775
KRaken <kuzembay@gmail.com>
2022-07-21 14:37:28 +0600

    fix: sql query ;

--- internal/user/repository/sql_queries.go
+++ internal/user/repository/sql_queries.go
@@ -7,11 +7,11 @@ const (
 			date_of_birth,is_private,avatar,nickname,about) 
 		VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10);`
 	getUserByEmailQuery = `
-		SELECT * FROM users WHERE email=$1`
+		SELECT * FROM users WHERE email=$1;`
 	getUserIDByEmailQuery = `
-		SELECT user_id FROM users WHERE email=$1`
+		SELECT user_id FROM users WHERE email=$1;`
 	getUserByIDQuery = `
-		SELECT * FROM users WHERE user_id=$1`
+		SELECT * FROM users WHERE user_id=$1;`
 )
 
 const (
