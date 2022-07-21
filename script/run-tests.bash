92ff722bb96701f212c9fa1dc817b2dd80457e66
KRaken <kuzembay@gmail.com>
2022-07-22 01:29:53 +0600

    feat: added scripts:

---
+++ script/run-tests.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export BACKEND_PORT=3003
+export MIGRATION=true
+export DB_NAME=test.db
+export RUN_ENV=test
+
+(trap 'kill 0' SIGINT; 
+cd backend && go run cmd/main.go & 
+cd test && npm run test && kill -9 `lsof -t -i:3003` && rm ../backend/test.db)
